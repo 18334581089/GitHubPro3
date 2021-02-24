2b442f1f7e08b114fac47f3f7db3b2a1e85b05eb
duanyuxiang <18334581089@139.com>
2021-02-24 19:11:23 +0800

    [Type]: [JIRA-ID]: [Description]:

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, Image } from '@tarojs/components'
+import Taro from '@tarojs/taro'
+import { View, Image } from '@tarojs/components'
 import './index.scss'
 
 import ima1 from './../../assets/stormtroopocat.png'
@@ -8,7 +9,27 @@ export default class Index extends Component {
 
   componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount () {
+    let BASE_URL = 'https://api-github.naotu.online'
+
+    request(BASE_URL + `?type=repo&language=&since=daily`, {}, 'GET')
+    function request (
+      url: string,
+      data?: any,
+      headers = {}
+    ) {
+      Taro.request({
+        url,
+        data,
+        method: 'GET',
+        header: {
+          Authorization: '',
+          ...headers
+        }
+      }).then(res => console.log(res)).catch(res => console.log(res))
+    }
+
+  }
 
   componentWillUnmount () { }
 
